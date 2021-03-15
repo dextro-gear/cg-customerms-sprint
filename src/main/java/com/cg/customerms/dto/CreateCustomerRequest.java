1aaa1e5f32989a6002a2fd89875c5ffcbab07dc4
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-15 14:21:36 +0530

    Rename AddCustomerRequest to CreateCustomerRequest

---
+++ src/main/java/com/cg/customerms/dto/CreateCustomerRequest.java
@@ -0,0 +1,22 @@
+package com.cg.customerms.dto;
+
+public class CreateCustomerRequest {
+    private String name;
+
+    public CreateCustomerRequest() {
+    }
+
+    public CreateCustomerRequest(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
