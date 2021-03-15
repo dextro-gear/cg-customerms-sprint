8f49e81d575fb598fb115637f8ff2f015c7ec5df
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-15 15:52:10 +0530

    Add DTO request object to update customer balance

---
+++ src/main/java/com/cg/customerms/dto/UpdateCustomerBalanceRequest.java
@@ -0,0 +1,27 @@
+package com.cg.customerms.dto;
+
+public class UpdateCustomerBalanceRequest {
+    long id;
+    double amount;
+
+    public UpdateCustomerBalanceRequest(long id, double amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
