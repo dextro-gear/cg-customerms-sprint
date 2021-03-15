cd3e130b4f26da77af894447cdf0ef134138b70c
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-15 15:53:40 +0530

    Add REST controller for item management

---
+++ src/main/java/com/cg/controllers/ItemsRESTController.java
@@ -0,0 +1,16 @@
+package com.cg.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/items")
+@RestController
+public class ItemsRESTController {
+
+    /*
+    * 1. Add items
+    * 2. Buy items
+    * 3. Get item details
+    */
+
+}
