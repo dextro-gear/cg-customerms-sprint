19ec813bdbc568e78c6d09e1dea805049f958e40
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-15 15:50:21 +0530

    Update customer response JSON object to contain account details

--- src/main/java/com/cg/customerms/dto/CustomerJSON.java
+++ src/main/java/com/cg/customerms/dto/CustomerJSON.java
@@ -1,15 +1,19 @@
 package com.cg.customerms.dto;
 
+import com.cg.customerms.entities.Account;
+
 public class CustomerJSON {
 
     private long customerID;
     private String customerName;
+    private AccountJSON account;
 
     public CustomerJSON(){}
 
-    public CustomerJSON(long customerID, String customerName) {
+    public CustomerJSON(long customerID, String customerName, Account account) {
         this.customerID = customerID;
         this.customerName = customerName;
+        this.account = new AccountJSON(account.getAccountID(), account.getBalance());
     }
 
     public long getCustomerID() {
@@ -26,5 +30,13 @@ public class CustomerJSON {
 
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
+    }
+
+    public AccountJSON getAccount() {
+        return account;
+    }
+
+    public void setAccount(AccountJSON account) {
+        this.account = account;
     }
 }
