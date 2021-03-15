f678a92279de800cf85e660dddccfa0ebbe851b3
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-15 14:24:10 +0530

    Open additional endpoints for retrieving customers and items bought by customers

--- src/main/java/com/cg/controllers/CustomerRESTController.java
+++ src/main/java/com/cg/controllers/CustomerRESTController.java
@@ -1,10 +1,13 @@
 package com.cg.controllers;
 
-import com.cg.customerms.dto.AddCustomerRequest;
+import com.cg.customerms.dto.BoughtItemsJSON;
+import com.cg.customerms.dto.CreateCustomerRequest;
 import com.cg.customerms.dto.CustomerJSON;
+import com.cg.customerms.dto.UpdateCustomerRequest;
 import com.cg.customerms.services.CustomerServiceImpl;
 import com.cg.customerms.utilities.CustomerUtilities;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/customer")
@@ -18,19 +21,31 @@ public class CustomerRESTController {
     CustomerUtilities customerUtil;
 
     /*
-    * 1. Add customer.
-    * 2. Update Customer
-    * 3. Find Customer
-    * 4. Retrieve items bought by customer.
+    * 1. Add customer. ---> DONE
+    * 2. Update Customer ---> DONE
+    * 3. Find Customer ---> DONE
+    * 4. Retrieve items bought by customer. ---> DONE
     * 5. Retrieve all customers. (Optional)
     */
 
     // Add customer
     @PostMapping("/add")
-    public CustomerJSON addCustomer(@RequestBody AddCustomerRequest request) {
+    public CustomerJSON addCustomer(@RequestBody CreateCustomerRequest request) {
         return customerUtil.toJSON(customerService.createCustomer(request.getName()));
     }
 
-    @PostMapping("/update")
+    @PutMapping("/update")
+    public CustomerJSON updateCustomer(@RequestBody UpdateCustomerRequest request){
+        return customerUtil.toJSON(customerService.updateCustomer(request.getId(), request.getName()));
+    }
 
+    @GetMapping("/get/byID/{id}")
+    public CustomerJSON findCustomer(@PathVariable("id") long customerID){
+        return customerUtil.toJSON(customerService.findByID(customerID));
+    }
+
+    @GetMapping("/getItemsBoughtByCustomer/{id}")
+    public BoughtItemsJSON retrieveItemsBoughtByCustomer(@PathVariable("id") long customerID){
+        return customerUtil.toJSON(customerService.itemsBoughtByCustomer(customerID));
+    }
 }
