041be2c0023b5a1e8492c679e47e4dff4560df57
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-15 14:27:15 +0530

    Reset ConsoleUI

--- src/main/java/com/cg/customerms/ui/ConsoleUI.java
+++ src/main/java/com/cg/customerms/ui/ConsoleUI.java
@@ -33,20 +33,20 @@ public class ConsoleUI {
 
 		System.out.println("ConsoleUI started...");
 
-//		List<Customer> customers = new ArrayList<Customer>();
-//		customers.add(customerService.createCustomer("Raja"));
-//		customers.add(customerService.createCustomer("Tanisha"));
-//		customers.add(customerService.createCustomer("Jatin"));
-//		customers.add(customerService.createCustomer("Ayesha"));
-//		customers.add(customerService.createCustomer("Dhruv"));
-//		customers.add(customerService.createCustomer("Shipra"));
-//		displayCustomers(customers);
-//
-//		itemService.create(10, "chips");
-//		itemService.create(30, "cola");
-//		itemService.create(40000, "washing machine");
-//		itemService.create(13000, "note 7 pro");
-//		itemService.create(70, "chocolate");
+		List<Customer> customers = new ArrayList<Customer>();
+		customers.add(customerService.createCustomer("Raja"));
+		customers.add(customerService.createCustomer("Tanisha"));
+		customers.add(customerService.createCustomer("Jatin"));
+		customers.add(customerService.createCustomer("Ayesha"));
+		customers.add(customerService.createCustomer("Dhruv"));
+		customers.add(customerService.createCustomer("Shipra"));
+		displayCustomers(customers);
+
+		itemService.create(10, "chips");
+		itemService.create(30, "cola");
+		itemService.create(40000, "washing machine");
+		itemService.create(13000, "note 7 pro");
+		itemService.create(70, "chocolate");
 //
 //		itemService.buyItem("6729398980", 2);
 //		itemService.buyItem("0028255068", 4);
